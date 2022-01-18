b193eaa8e51345219609b1e8277ec2271fb87379
Matt Bentley <mbentley@mbentley.net>
2022-01-18 09:06:12 -0500

    Fixed tsmuxer build; skip static build
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- build-tsmuxer.sh
+++ build-tsmuxer.sh
@@ -3,19 +3,24 @@
 set -e
 set -x
 
-# last commit that works (until I have figured out https://github.com/justdan96/tsMuxer/issues/543)
-git clone --depth 1 --branch nightly-2022-01-11-02-11-13 https://github.com/justdan96/tsMuxer.git
-
-# doesn't work (see above)
-#git clone --depth 1 https://github.com/justdan96/tsMuxer.git
+git clone --depth 1 https://github.com/justdan96/tsMuxer.git
 
 cd tsMuxer
 
 # set build args
 export MAKEFLAGS="-j 4"
 
-# execute script from repo
-./rebuild_linux_docker.sh
+# commands directly taken from (https://github.com/justdan96/tsMuxer/blob/master/rebuild_linux_docker.sh) except for disabling the static build
+rm -rf build
+mkdir build
+cd build
+cmake -DTSMUXER_STATIC_BUILD=OFF -DFREETYPE_LDFLAGS=png ../
+make
+cp tsMuxer/tsmuxer ../bin/tsMuxeR
+cd ..
+rm -rf build
+ls ./bin/tsMuxeR
 
+# inspect the file and check the version
 file ./bin/tsMuxeR
 ./bin/tsMuxeR --version
